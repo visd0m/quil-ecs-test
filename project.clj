1fbf39d6e6a10d88a975da12ff001b002fce6e01
Domenico Visconti <domenico.visconti@prima.it>
2025-01-27 18:30:04 +0100

    Add main in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject quil-test "0.1.0-SNAPSHOT"
   :description "quil test project"
+  :main quil-test.core
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [quil "2.8.0"]])
